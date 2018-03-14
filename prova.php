0c5593ed2d39c2c4b62b23eb9abb04ecd7959dce
mpoggi64 <mpoggi64@gmail.com>
2018-03-14 14:27:24 +0100

    prova.php
    php numeri casuali

---
+++ prova.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head><title>Numeri casuali</title></head>
+<body>
+<h1>Numeri generati</h1>
+<?php
+//genera tre numeri casuali tra 1 e 90 
+echo rand(1, 90) . “<br>”;
+echo rand(1, 90)	. “<br>”;
+echo rand(1, 90)	. “<br>”;
+echo “La data di oggi: ”.Date(“d-m-Y”);
+?>
+</body>
+</html>
